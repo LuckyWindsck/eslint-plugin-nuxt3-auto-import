d7f936e6a4d21e159a18a785e1258d927b44aa2f
LuckyWind_sck <luckywindsck@gmail.com>
2022-07-02 23:50:56 +0900

    feat: add nuxt3 auto imported functions to ESLint global

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,25 @@
-"use strict";
-
 module.exports = {
   root: true,
   extends: [
-    "eslint:recommended",
-    "plugin:eslint-plugin/recommended",
-    "plugin:node/recommended",
+    'eslint:recommended',
+    'plugin:eslint-plugin/recommended',
+    'plugin:node/recommended',
+    'airbnb-base',
   ],
   env: {
+    es6: true,
     node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   overrides: [
     {
-      files: ["tests/**/*.js"],
+      files: ['tests/**/*.js'],
       env: { mocha: true },
     },
   ],
+  rules: {
+  },
 };
